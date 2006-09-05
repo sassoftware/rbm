cb1f10753c76c1ea85bc78b15f5d7996d3bf6b77
Tim Gerla <http://issues.rpath.com/>
2006-09-05 10:18:24 -0400

    add tests

---
+++ tests/__init__.py
@@ -0,0 +1,15 @@
+import cherrypy
+import os
+import raatest
+
+class webPluginTest(raatest.webTest):
+    def __init__(
+        self, module = None, init = True, preInit = None, preConst = None):
+
+        def func(rt):
+            cherrypy.root.servicecfg.pluginDirs = ["..", "raa-plugin"]
+            if preInit:
+                preInit(rt)
+
+        return raatest.webTest.__init__(
+            self, module=module, init=init, preInit=func, preConst=preConst)
---
+++ tests/diskusage/tests/__init__.py
@@ -0,0 +1,25 @@
+#
+# Copyright (C) 2006 rPath, Inc.
+# All rights reserved.
+#
+import cherrypy
+import raapluginstest
+import raatest
+
+from tests import webPluginTest
+raaFramework = webPluginTest()
+raaFramework.pseudoroot = cherrypy.root.diskusage.DiskUsage
+
+
+class DiskUsageTest(raatest.rAATest):
+    def test_method(self):
+        "the index method should return a string called now"
+        import types
+        result = self.callWithIdent(raaFramework.pseudoroot.index)
+        assert type(result) == types.DictType
+
+    def test_indextitle(self):
+        "The mainpage should have the right title"
+        self.requestWithIdent("/diskusage/DiskUsage/")
+        print cherrypy.response.body[0]
+        assert "<title>disk usage</title>" in cherrypy.response.body[0].lower()
