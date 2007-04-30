72706dec92997dc7035c0f3aed16c6664ef3e4dc
Stu Gott <http://issues.rpath.com/>
2007-04-30 17:21:18 -0400

    correct backup plugin to only save programatic changes from /etc/fstab

--- scripts/backup.py
+++ scripts/backup.py
@@ -2,12 +2,15 @@
 
 import os
 import pwd
+import re
 import shutil
 import sys
 import fcntl
 
 dbPath = os.path.join(os.path.sep, 'srv', 'conary', 'sqldb')
 tmpDbPath = dbPath + '-tmp'
+tmpFsPath = os.path.join(os.path.sep, 'srv', 'conary', 'fstab')
+apacheUID, apacheGID = pwd.getpwnam('apache')[2:4]
 
 class DBLock(object):
     def __init__(self, path):
@@ -21,14 +24,31 @@ class DBLock(object):
         self.file.close()
 
 def clean():
-    if os.path.exists(tmpDbPath):
-        os.unlink(tmpDbPath)
+    for path in (tmpDbPath, tmpFsPath):
+        if os.path.exists(path):
+            os.unlink(path)
 
 def backup(out = sys.stdout):
-    print >> out, os.path.join(os.path.sep, 'etc', 'fstab')
     for bkpFile in ('conaryver', 'repository.cnr', 'repository-custom.cnr',
                     'repository-generated.cnr', 'logs'):
         print >> out, os.path.join(os.path.sep, 'srv', 'conary', bkpFile)
+
+    # now back up any programmaticly changed lines from /etc/fstab
+    try:
+        f = open(os.path.join(os.path.sep, 'etc', 'fstab'))
+    except IOError:
+        fsData = ''
+    else:
+        fsData = f.read()
+        f.close()
+
+    entry = re.findall('.*\s/srv/conary/contents\s.*', fsData)
+    entry = entry and entry[0] or ''
+    f = open(tmpFsPath, 'w')
+    f.write(entry)
+    f.close()
+    print >> out, tmpFsPath
+    os.chown(tmpFsPath, apacheUID, apacheGID)
 
     # now back up the sqldb
     dbLock = DBLock(dbPath)
@@ -38,9 +58,25 @@ def backup(out = sys.stdout):
     finally:
         dbLock.release()
     print >> out, tmpDbPath
+    os.chown(tmpDbPath, apacheUID, apacheGID)
 
 def restore():
     shutil.move(tmpDbPath, dbPath)
+    os.chown(dbPath, apacheUID, apacheGID)
+    f = open(tmpFsPath)
+    entry = f.read()
+    f.close()
+    if entry:
+        f = open(os.path.join(os.path.sep, 'etc', 'fstab'), 'r+')
+        data = f.read()
+        if not re.findall('.*\s/srv/conary/contents\s.*', data):
+            if data and data[-1] != '\n':
+                entry = '\n' + entry
+            if entry[-1] != '\n':
+                entry += '\n'
+
+            f.write(entry)
+        f.close()
 
 def handle(func, *args, **kwargs):
     errno = 0
@@ -64,9 +100,6 @@ def usage(out = sys.stderr):
     print >> out, "    [c/clean]: clean up temporary files used during backup."
 
 def main(argv = sys.argv[1:]):
-    apacheUID, apacheGID = pwd.getpwnam('apache')[2:4]
-    os.setgid(apacheGID)
-    os.setuid(apacheUID)
     mode = (len(sys.argv) > 1) and sys.argv[1] or ''
     if mode in ('r', 'restore'):
         sys.exit(handle(restore))
