797ea56c14e33d5839066c3bca7b5f226240cf90
Michael Tharp <https://issues.rpath.com/>
2008-03-10 09:07:11 -0400

    Update backup scripts to deal with postgresql

--- scripts/backup.py
+++ scripts/backup.py
@@ -7,10 +7,17 @@ import shutil
 import sys
 import fcntl
 
+from conary.lib import util
+from conary.repository import netserver
+
+
 dbPath = os.path.join(os.path.sep, 'srv', 'conary', 'sqldb')
 tmpDbPath = dbPath + '-tmp'
 tmpFsPath = os.path.join(os.path.sep, 'srv', 'conary', 'fstab')
 apacheUID, apacheGID = pwd.getpwnam('apache')[2:4]
+
+cfg_path = '/srv/conary/repository.cnr'
+pg_dump_path = '/srv/conary/rapus.sql'
 
 class DBLock(object):
     def __init__(self, path):
@@ -24,7 +31,7 @@ class DBLock(object):
         self.file.close()
 
 def clean():
-    for path in (tmpDbPath, tmpFsPath):
+    for path in (tmpDbPath, tmpFsPath, pg_dump_path):
         if os.path.exists(path):
             os.unlink(path)
 
@@ -51,20 +58,49 @@ def backup(out = sys.stdout):
     os.chown(tmpFsPath, apacheUID, apacheGID)
 
     # now back up the sqldb
-    ### TODO: Fix this for postgresql support
-    dbLock = DBLock(dbPath)
-    dbLock.acquire()
-    try:
-        shutil.copy(dbPath, tmpDbPath)
-    finally:
-        dbLock.release()
-    print >> out, tmpDbPath
-    os.chown(tmpDbPath, apacheUID, apacheGID)
+    server_cfg = netserver.ServerConfig()
+    server_cfg.read(cfg_path)
+    rep_driver, rep_path = server_cfg.repositoryDB
+
+    if rep_driver == 'sqlite':
+        dbLock = DBLock(dbPath)
+        dbLock.acquire()
+        try:
+            shutil.copy(dbPath, tmpDbPath)
+        finally:
+            dbLock.release()
+        print >> out, tmpDbPath
+        os.chown(tmpDbPath, apacheUID, apacheGID)
+    elif rep_driver == 'postgresql':
+        util.execute('pg_dump -c --disable-triggers rapus >"%s"' % pg_dump_path
+        print >> out, pg_dump_path
+    else:
+        sys.exit("Don't know how to back up a '%s' database!" % rep_driver)
 
 def restore():
-    ### TODO: Fix this for postgresql support
-    shutil.move(tmpDbPath, dbPath)
-    os.chown(dbPath, apacheUID, apacheGID)
+    server_cfg = netserver.ServerConfig()
+    server_cfg.read(cfg_path)
+    rep_driver, rep_path = server_cfg.repositoryDB
+
+    if rep_driver == 'sqlite':
+        shutil.move(tmpDbPath, dbPath)
+        os.chown(dbPath, apacheUID, apacheGID)
+    elif rep_driver == 'postgresql':
+        # Try to create needed resources first. This will generally fail since
+        # these are created when the database is initialized, but we might
+        # be restoring onto a bare database.
+        os.system('psql -U postgres -d postgres -c "CREATE ROLE rapus '
+            'NOSUPERUSER CREATEDB NOCREATEROLE INHERIT LOGIN;" '
+            '>/dev/null 2>&1')
+        os.system('psql -U rapus -d postgres -c "CREATE DATABASE rapus '
+            'ENCODING \'UTF8\';" >/dev/null 2>&1')
+        os.system('createlang -U postgres plpgsql rapus >/dev/null 2>&1')
+
+        # Now do the restore
+        util.execute('psql -l <"%s" >/dev/null' % pg_dump_path)
+    else:
+        sys.exit("Don't know how to restore a '%s' database!" % rep_driver)
+
     f = open(tmpFsPath)
     entry = f.read()
     f.close()
