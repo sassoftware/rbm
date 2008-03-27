a015c8a4ed7c24b7d8bf2d54fb006bf08bd16a74
Scott Parkerson <http://issues.rpath.com/>
2008-03-27 17:41:16 -0400

    Update Service no longer requires explicit serverName setup (RBM-212)
    1. conaryserver rAPA plugin neutered to only have XML-RPC
       methods for {add,del}ServerName. Always returns True.
       Also, web interface now removed (and remants hidden).
    2. rbm_rc mod_python handler now uses repositoryName * rather than
       generate them from a list of serverName entries.
    3. We aren't using a generated config file anymore, so update
       all taghandlers, etc. to remove them on sight.

--- rcgenerator/rbm_rc.py
+++ rcgenerator/rbm_rc.py
@@ -9,8 +9,6 @@ import urlparse
 
 from mod_python import apache
 
-conaryrcPath = '/srv/www/html/conaryrc'
-
 def handler(req):
     if req.uri != '/conaryrc':
         # We only handle conaryrc
@@ -19,20 +17,6 @@ def handler(req):
     if req.method.upper() != 'GET':
         return apache.HTTP_BAD_REQUEST
 
-    if not os.path.isfile(conaryrcPath):
-        return apache.HTTP_NOT_FOUND
-
     req.content_type = 'text/plain'
-
-    f = open(conaryrcPath, 'r')
-    for line in f:
-        if line.startswith('repositoryMap '):
-            _, serverName, url = line.strip().split(' ', 2)
-            parsed = list(urlparse.urlparse(url))
-            parsed[1] = req.hostname
-            url = urlparse.urlunparse(parsed)
-            req.write('repositoryMap %s %s\n' % (serverName, url))
-        else:
-            req.write(line)
-
+    req.write('repositoryMap * https://%s/conary/' % req.hostname)
     return apache.OK
