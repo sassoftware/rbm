50f5d2762b2f6b1950b001062e5b65c4bf5a14d7
Matt Wilson <https://issues.rpath.com/>
2008-06-25 13:22:59 -0400

    look in the Conary repository database to determine what server names are actually housed there (RBM-256)

--- src/rbm_rc.py
+++ src/rbm_rc.py
@@ -6,6 +6,8 @@
 
 import os
 import urlparse
+from conary import dbstore
+from conary.repository.netrepos import netserver
 
 from mod_python import apache
 
@@ -17,6 +19,26 @@ def handler(req):
     if req.method.upper() != 'GET':
         return apache.HTTP_BAD_REQUEST
 
+    cfg = netserver.ServerConfig()
+    cfg.read('/srv/conary/repository.cnr')
+    db = dbstore.connect(cfg.repositoryDB[1], driver = cfg.repositoryDB[0])
+    cu = db.cursor()
+    cu.execute("""SELECT LABEL FROM Labels WHERE EXISTS
+                      ( SELECT labelId FROM LabelMap
+                            JOIN Nodes USING(itemId, branchId)
+                            JOIN Instances USING(itemId, versionId)
+                        WHERE isPresent=1
+                            AND LabelMap.labelId = Labels.labelId )""")
+    serverNames = set()
+    for label, in cu:
+        if '@' not in label:
+            continue
+        name = label.split('@', 1)[0]
+        serverNames.add(name)
+
     req.content_type = 'text/plain'
-    req.write('repositoryMap * https://%s/conary/' % req.hostname)
+
+    for name in serverNames:
+        req.write('repositoryMap %s https://%s/conary/\n' % (name, req.hostname))
+
     return apache.OK
