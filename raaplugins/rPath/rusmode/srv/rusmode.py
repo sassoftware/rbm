13b2463a449b4c42d511fe4546c51c711ef07979
Elliot Peele <https://issues.rpath.com/>
2011-02-01 17:45:24 -0500

    move memcache configuration into global repository config and increase timeout to 1 hour (RBL-8205)

--- raaplugins/rPath/rusmode/srv/rusmode.py
+++ raaplugins/rPath/rusmode/srv/rusmode.py
@@ -33,8 +33,6 @@ class RUSMode(rAASrvPlugin):
             print >> fobj, "proxyContentsDir %s" % self.proxyDir
             print >> fobj, "conaryProxy http http://%s" % rbaHostname
             print >> fobj, "conaryProxy https https://%s" % rbaHostname
-            print >> fobj, "memCached localhost:11211"
-            print >> fobj, "memCachedTimeout 600"
 
             fobj.commit()
 
