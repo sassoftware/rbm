f7bb5a58774cdb6ecdfae12f7919ae2e3dfd8f8d
Michael Tharp <michael.tharp@sas.com>
2013-06-19 13:47:36 +0000

    Update conary migration script to deal with extended config

--- upsrv/conary_schema.py
+++ upsrv/conary_schema.py
@@ -1,32 +1,29 @@
 #!/usr/bin/python
-
-# Copyright (c) 2006 rPath, Inc
-# All rights reserved
+#
+# Copyright (c) SAS Institute Inc.
+#
 
 import sys
 import os
 import pwd
 from conary.server import schema
 from conary.lib import cfgtypes, tracelog
-from conary.repository.netrepos.netserver import ServerConfig
 from conary import dbstore
+
+from .config import UpsrvConfig
 
 class SimpleFileLog(tracelog.FileLog):
     def printLog(self, level, msg):
         self.fd.write("%s\n" % msg)
 
-cnrPath = '/srv/conary/repository.cnr'
-
-cfg = ServerConfig()
+try:
+    cfg = UpsrvConfig.load()
+except cfgtypes.CfgEnvironmentError:
+    print "Error reading config file"
+    sys.exit(1)
 
 tracelog.FileLog = SimpleFileLog
 tracelog.initLog(filename='stderr', level=2)
-
-try:
-    cfg.read(cnrPath)
-except cfgtypes.CfgEnvironmentError:
-    print "Error reading %s" % cnrPath
-    sys.exit(1)
 
 db = dbstore.connect(cfg.repositoryDB[1], cfg.repositoryDB[0])
 schema.loadSchema(db, doMigrate=True)
