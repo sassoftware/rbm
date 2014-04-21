57c59e751e45ac6562b81907e97694c6e953af3b
Mihai Ibanescu <mihai.ibanescu@sas.com>
2014-04-21 14:15:01 -0400

    Removed more python 2.7-isms (APPENG-2722)

--- upsrv_test/record_test.py
+++ upsrv_test/record_test.py
@@ -252,8 +252,9 @@ update baz=/invalid.version.string@ns:1
         rectime = datetime.datetime.strptime(rec['updated_time'],
                 "%Y-%m-%dT%H:%M:%S.%f")
         delta = now - rectime
-        self.assertTrue(0 <  delta.total_seconds())
-        self.assertTrue(delta.total_seconds() < 2)
+        totalSeconds = 86400 * delta.days + delta.seconds
+        self.assertTrue(0 <  totalSeconds)
+        self.assertTrue(totalSeconds < 2)
 
         self.assertEquals(req.getConaryClient._mock.popCall(),
                 ((), (('entitlements', ['aaa', 'bbb']),)))
