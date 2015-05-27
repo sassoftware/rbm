3e224fab936b6f29e719d69481c9b406da17f3ed
Michael Tharp <michael.tharp@sas.com>
2015-05-27 19:46:12 +0000

    Relicense as Apache 2 (APPENG-3588)

--- upsrv_test/record_test.py
+++ upsrv_test/record_test.py
@@ -1,6 +1,18 @@
 #
 # Copyright (c) SAS Institute Inc.
 #
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
 
 
 import crypt
@@ -396,4 +408,3 @@ update baz=/invalid.version.string@ns:1
         self.assertEqual(
                 [x['uuid'] for x in resp.json['records']],
                 [recordsData[6][0], recordsData[7][0], recordsData[8][0]])
-
